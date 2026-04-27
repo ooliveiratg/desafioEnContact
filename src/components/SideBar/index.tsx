409e478b14bc9ebad6787f10febbd174aaba5b70
ooliveiratg <menezesth16@gmail.com>
2026-04-26 21:50:31 -0300

    feat(menuAccount): Adding a connection to the API related to the menu.

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -7,16 +7,45 @@ import {
   SidebarGroupLabel,
   SidebarHeader,
   SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
 } from "@/ui/sidebar";
 import { useTheme } from "next-themes";
 import { FaRegStar } from "react-icons/fa";
 import { HiMiniUsers } from "react-icons/hi2";
 import { RxExit } from "react-icons/rx";
 import { useNavigate } from "react-router-dom";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "../ui/collapsible";
+import { MenusAccount } from "@/services/menusAccount";
+import { toast } from "sonner";
+import { useEffect, useState } from "react";
+import type { IMenusAccount } from "@/interfaces/services/interface";
 
 export function AppSidebar() {
   const { theme } = useTheme();
-  const router = useNavigate()
+  const router = useNavigate();
+  const [menuData, setMenuData] = useState<IMenusAccount[] | undefined>(
+    undefined,
+  );
+
+  useEffect(() => {
+    async function fetchMenu() {
+      const response = await MenusAccount();
+      if (!response.success) {
+        toast.error(response.message || "Erro ao buscar os menus");
+        return;
+      }
+      setMenuData(response.data);
+    }
+
+    fetchMenu();
+  }, []);
   return (
     <Sidebar>
       <SidebarHeader>
@@ -44,24 +73,47 @@ export function AppSidebar() {
             <SidebarGroupLabel className="text-[#94A3B8] text-[12px] uppercase font-bold">
               Accounts
             </SidebarGroupLabel>
-            <SidebarGroup className="flex flex-col gap-[12px] pt-[12px]">
-              <SidebarMenu className="flex flex-row w-[full] justify-between items-center">
-                <SidebarMenu className="flex flex-row gap-[12px] items-center">
-                  <HiMiniUsers size={18} />
-                  <h3 className="font-semibold text-[13px] text-white">
-                    Conta 1
-                  </h3>
-                </SidebarMenu>
-                <div className="bg-white/10 px-[8px] py-[2px] rounded-full">
-                  <p className="font-bold text-[12px]">62</p>
-                </div>
-              </SidebarMenu>
-            </SidebarGroup>
+            {menuData &&
+              menuData.map((itens, index) => (
+                <Collapsible key={index} defaultOpen className="pt-[12px]">
+                  <CollapsibleTrigger asChild className="flex flex-col  min-w-full ">
+                      <SidebarMenuButton className="flex flex-row w-[full] gap-[12px] justify-between items-center">
+                        <SidebarMenu className="flex flex-row gap-[12px] items-center">
+                          <HiMiniUsers size={18} />
+                          <h3 className="font-semibold text-[13px] text-white">
+                            {itens.name}
+                          </h3>
+                        </SidebarMenu>
+                        <div className="bg-white/10 px-[8px] rounded-full">
+                          <p className="font-bold text-[12px]">62</p>
+                        </div>
+                      </SidebarMenuButton>
+                  </CollapsibleTrigger>
+
+                  <CollapsibleContent>
+                    {itens.subMenus.map((subItens, subIndex) => (
+                      <SidebarMenuSub key={subIndex}>
+                        <SidebarMenuSubItem>
+                          <SidebarMenuSubButton>
+                            <span>{subItens.name} </span>
+                            <span className="ml-auto">15</span>
+                          </SidebarMenuSubButton>
+                        </SidebarMenuSubItem>
+                      </SidebarMenuSub>
+                    ))}
+                  </CollapsibleContent>
+                </Collapsible>
+              ))}
           </SidebarGroup>
+
           <SidebarFooter className="flex flex-col">
             <div className="flex w-full h-[1px] bg-white mb-[12px]" />
             <SidebarMenu className="flex flex-row gap-[12px] items-center pt-[12px]">
-              <Button variant={"ghost"} className="cursor-pointer" onClick={() => router("/")}>
+              <Button
+                variant={"ghost"}
+                className="cursor-pointer"
+                onClick={() => router("/")}
+              >
                 <RxExit size={18} color="#FF0000" />
                 <h3 className="font-semibold text-[13px] text-[#FF0000]">
                   Sair
---
+++ src/interfaces/services/interface.ts
@@ -0,0 +1,18 @@
+export interface IMenusAccount {
+  id: number;
+  name: string;
+  subMenus: [
+    {
+      id: number;
+      name: string;
+    },
+  ];
+}
+
+export interface IAccount {
+  id: number;
+  name: string;
+  subject: string;
+  owner: string;
+  users: string[];
+}
